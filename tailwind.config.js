53a753ef88bb9cb84ca1bb8e26f221e712c9067b
E. M. Greeff <emgreeff@datadash.cloud>
2023-09-02 05:01:30 -0600

    svelte-check found 0 errors and 0 warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,37 @@
-import { join } from 'path'
+import { join } from "path";
 
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import { skeleton } from "@skeletonlabs/tw-plugin";
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'modern',
-						enhancements: true,
-					},
-					{
-						name: 'wintry',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(
+      require.resolve("@skeletonlabs/skeleton"),
+      "../**/*.{html,js,svelte,ts}",
+    ),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms,
+    typography,
+    skeleton({
+      themes: {
+        preset: [
+          {
+            name: "modern",
+            enhancements: true,
+          },
+          {
+            name: "wintry",
+            enhancements: true,
+          },
+        ],
+      },
+    }),
+  ],
 };
